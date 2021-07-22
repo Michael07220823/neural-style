a55d4589c6e9b68f401af04244fc0971883b8256
Michael07220823 <michael31703@gmail.com>
2021-07-22 09:04:50 +0800

    Add resize_image.py to resize image size.

---
+++ resize_image.py
@@ -0,0 +1,18 @@
+import os
+import sys
+import cv2
+import imutils
+
+image_path = sys.argv[1]
+image_split = image_path.split(".")
+new_image_path = image_split[0] + "-resize.{}".format(image_split[-1])
+
+if not os.path.exists(image_path):
+    print("Image not existsed !")
+
+image = cv2.imread(image_path)
+resize_image = imutils.resize(image, width=500)
+cv2.imwrite(new_image_path, resize_image)
+
+print("Resized {} successfully !".format(image_path))
+print("Saved image to {}.".format(new_image_path))
