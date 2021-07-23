2aab6efe7f29f1afe51ff5db6c4c0f0db69adb6a
Michael07220823 <michael31703@gmail.com>
2021-07-23 08:42:51 +0800

    Add shell input、style and output arguments.

--- transfer_style.sh
+++ transfer_style.sh
@@ -1,15 +1,19 @@
+# Usage
+# Format: sh transfer_style.sh INPUT_IMAGE STYLE_IMAGE OUTPUT_IMAGE
+# sh transfer_style.sh examples/input/asia_unversity.jpg style/style35.jpg examples/output/asia_unversity-style35-2.jpg
+
 # 原始影像。
-CONTENT=examples/input/eagle-5.jpg
+CONTENT=$1
 # 一個或多個風格影像。
-STYLES="style/style35.jpg"
+STYLES=$2
 # 輸出影像。
-OUTPUT=examples/output/eagle-5-style35-19.jpg
+OUTPUT=$3
 
 # learning rate數字越低，風格轉移的越精細。
 LEARNING_RATE=3.0
 
 # Iterations數字越高，風格轉移時間越長。值要>=1000風格轉換的效果越好。
-ITERATIONS=1125
+ITERATIONS=1100
 
 # 輸入影像的佔比，預設值為5.0。
 CONTENT_WEIGHT=5.0
@@ -21,10 +25,10 @@ STYLE_WEIGHT=500
 STYLE_SCALES="1.0"
 
 # Content weight blend默認值1.0，樣式傳輸嘗試保留更精細的顆粒內容詳細資訊。值應在 [0.0; 1.0] 範圍內。
-CONTENT_WEIGHT_BLEND=0.35
+CONTENT_WEIGHT_BLEND=1.0
 
 # Style layer weight exp調整樣式轉移的「抽象」程度，數字越小越精細，越大越粗略，預設值為 1.0。
-STYLE_LAYER_WEIGHT_EXP=3.0
+STYLE_LAYER_WEIGHT_EXP=0.5
 
 # Pooling一般最大池往往具有更精細的細節樣式傳輸，但在低頻率細節級別可能會遇到問題，因此建議改成avg。
 POOLING=max
